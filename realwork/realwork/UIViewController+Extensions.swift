0e65a9f9e2ceed19b704fc49911f053af629221d
Nathan Ngobale <nngobale@DVTL101621.local>
2020-02-10 10:03:07 +0200

    Redid UI interface

---
+++ realwork/realwork/UIViewController+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Extensions.swift
+//  realwork
+//
+//  Created by Nathan Ngobale on 2020/02/10.
+//  Copyright © 2020 Nathan Ngobale. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    @IBAction func unwind(_  segue: UIStoryboardSegue) {}
+}
--- realwork/realwork/ViewController.swift
+++ realwork/realwork/ViewController.swift
@@ -9,21 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var btn_Google: UIButton!
-    
-    @IBOutlet weak var btn_SignUp: UIButton!
-    
-    @IBOutlet weak var img_Background: UIImageView!
+    @IBOutlet weak var btn_continue: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        btn_Google.layer.cornerRadius = 30.0
-        btn_Google.layer.masksToBounds = true
-        btn_SignUp.layer.cornerRadius = 30.0
-        btn_SignUp.layer.masksToBounds = true
-        
-        img_Background.image = UIImage(named: "first")
         // Do any additional setup after loading the view.
     }
 
