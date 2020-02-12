d6c1654360555fc799363500f05f6465541114e1
Nathan Ngobale <nngobale@DVTL101621.local>
2020-02-12 11:46:25 +0200

    Added SwiftLint to project

--- realwork/realwork/ViewController.swift
+++ realwork/realwork/ViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var btn_continue: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
