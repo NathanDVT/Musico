a3933f4da4c79f124aedad0f80af482466a94492
Nathan Ngobale <nngobale@DVTL101621.local>
2020-02-17 15:47:08 +0200

    resolved some of the warnings

--- realwork/realwork/DesignableView.swift
+++ realwork/realwork/DesignableView.swift
@@ -10,25 +10,25 @@ import UIKit
 
 @IBDesignable
 class DesignableView: UIView {
-    @IBInspectable var shadowColor: UIColor = UIColor.clear{
-        didSet{
+    @IBInspectable var shadowColor: UIColor = UIColor.clear {
+        didSet {
             layer.shadowColor = shadowColor.cgColor
         }
     }
-    
+
     @IBInspectable var shadowRadius: CGFloat = 0 {
         didSet {
             layer.shadowRadius = shadowRadius
         }
     }
-    
+
     @IBInspectable var shadowOpacity: CGFloat = 0 {
         didSet {
             layer.shadowOpacity = Float(shadowOpacity)
         }
     }
-    
-    @IBInspectable var shadowOffsetY: CGFloat = 0{
+
+    @IBInspectable var shadowOffsetY: CGFloat = 0 {
         didSet {
             layer.shadowOffset.height = shadowOffsetY
         }
