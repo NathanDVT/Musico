1d6c55e8354cb86289aa59e3ab86e316c0efeaf4
Nathan Ngobale <nngobale@DVTL101621.local>
2020-02-21 08:16:04 +0200

    Removed unused code

--- realwork/realwork/TableCell.swift
+++ realwork/realwork/TableCell.swift
@@ -14,16 +14,4 @@ class TableCell: UITableViewCell {
     @IBOutlet weak var durationLbl: UILabel!
     @IBOutlet weak var titleLbl: UILabel!
     @IBOutlet weak var imgView: UIImageView!
-
-//    override func awakeFromNib() {
-//        super.awakeFromNib()
-//        // Initialization code
-//    }
-
-//    override func setSelected(_ selected: Bool, animated: Bool) {
-//        super.setSelected(selected, animated: animated)
-//
-//        // Configure the view for the selected state
-//    }
-
 }
