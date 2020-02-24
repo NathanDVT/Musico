598614ae7836f533b22bd75ed69732eb6acfa276
Nathan Ngobale <nngobale@DVTL101621.local>
2020-02-24 08:41:01 +0200

    Saving work remotely

--- realwork/realwork/TableCell.swift
+++ realwork/realwork/TableCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVKit
 
 class TableCell: UITableViewCell {
 
@@ -14,4 +15,27 @@ class TableCell: UITableViewCell {
     @IBOutlet weak var durationLbl: UILabel!
     @IBOutlet weak var titleLbl: UILabel!
     @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    var audioPlayer = AVAudioPlayer()
+    
+
+    @IBAction func musicActionPressed(_ sender: Any) {
+//        print("pressed play")
+//        var player: AVPlayer!
+//        let url  = URL.init(string:   "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview118/v4/94/25/9c/94259c23-84ee-129d-709c-577186cbe211/mzaf_5653537699505456197.plus.aac.p.m4a")
+//
+//        let playerItem: AVPlayerItem = AVPlayerItem(url: url!)
+//        player = AVPlayer(playerItem: playerItem)
+//
+//        let playerLayer = AVPlayerLayer(player: player!)
+//
+//        playerLayer.frame = CGRect(x: 0, y: 0, width: 10, height: 50)
+//        self.view.layer.addSublayer(playerLayer)
+//        player.play()
+
+    }
+
+
+
 }
--- realwork/realwork/ViewController.swift
+++ realwork/realwork/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
     let defaultSession = URLSession(configuration: .default)
@@ -14,6 +15,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
     }
 }
 
