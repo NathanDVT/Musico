36ad9b18d5c43f468e35731fcde00448678427bb
NathanDVT <nngobale@jhb.dvt.co.za>
2020-03-19 00:34:13 +0200

    Improved bitrise standards

--- realwork/realworkUITests/Helpers/UITestScreenNavigators.swift
+++ realwork/realworkUITests/Helpers/UITestScreenNavigators.swift
@@ -10,6 +10,7 @@ import XCTest
 
 extension XCUIApplication {
     func navigateStartToOnBoarding() {
+        XCUIDevice.shared.orientation = .portrait
         self.buttons["Onboarding"].tap()
         sleep(1)
     }
@@ -18,6 +19,7 @@ extension XCUIApplication {
         navigateStartToOnBoarding()
         swipeLeft()
         swipeLeft()
+        sleep(1)
         buttons["Proceed"].tap()
     }
 }
