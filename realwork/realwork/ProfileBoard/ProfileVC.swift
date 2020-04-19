e2434f5743e7ecde03e3c246c0e5444ad8ddd389
NathanDVT <nngobale@jhb.dvt.co.za>
2020-04-19 17:42:52 +0200

    Integrated functionality to profile screen

--- realwork/realwork/ProfileBoard/ProfileVC.swift
+++ realwork/realwork/ProfileBoard/ProfileVC.swift
@@ -7,24 +7,69 @@
 //
 
 import UIKit
+import NLibrary
+import FirebaseAnalytics
+import Crashlytics
 
 class ProfileVC: UIViewController {
 
+    @IBOutlet weak var noplaylistsUILabel: UILabel!
+    @IBOutlet weak var emailUILabel: UILabel!
+    @IBOutlet weak var genderUILabel: UILabel!
+    @IBOutlet weak var usernameUILabel: UILabel!
+     @IBOutlet weak var profileImage: UIImageView!
+    lazy var profileViewModel: ProfileViewModelProtocol = ProfileViewModel(viewController: self, repo: ProfileRepo())
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUpUIComponents()
+        fetchProfileData()
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setUpUIComponents() {
+        profileImage.layer.cornerRadius = profileImage.frame.size.width/2
+        profileImage.layer.borderColor = UIColor.white.cgColor
+        profileImage.layer.borderWidth = 2.0
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .organize, target: self, action: nil)
     }
-    */
 
+    @IBAction func logout(_ sender: Any) {
+        let dialogMessage = UIAlertController(title: "Logging Out",
+                                              message: "Are you sure you want log out?",
+                                              preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK",
+                               style: .default,
+                               handler: { (_) -> Void in
+             print("Ok button tapped")
+                                self.profileViewModel.logoutRequest()
+        })
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel) { (_) -> Void in
+        }
+        dialogMessage.addAction(okAction)
+        dialogMessage.addAction(cancel)
+        self.present(dialogMessage, animated: true, completion: nil)
+    }
+}
+
+extension ProfileVC: ProfileViewControllerProtocol {
+
+    func fetchProfileData() {
+        self.profileViewModel.getUserProfile()
+    }
+
+    func successfulRequest(profileModel: ProfileModel) {
+        self.emailUILabel.text = profileModel.email
+        self.genderUILabel.text = profileModel.gender.stringValue
+        self.noplaylistsUILabel.text = "\(profileModel.noPlaylists)"
+        self.usernameUILabel.text = profileModel.name
+    }
+
+    func successfulLogout() {
+        Analytics.logEvent("user_logged_out", parameters: nil)
+        let storyboard = UIStoryboard(name: "EntryBoard", bundle: nil)
+        let destinationVC = storyboard.instantiateViewController(withIdentifier:
+            "EntryBoardLoginID") as UIViewController
+            destinationVC.modalPresentationStyle = .fullScreen
+            destinationVC.modalTransitionStyle = .crossDissolve
+            present(destinationVC, animated: true, completion: nil)
+    }
 }
