90f53b978473ec285d594908ddf434135715b923
NathanDVT <nngobale@jhb.dvt.co.za>
2020-04-20 20:21:22 +0200

    Added Profile Tests

---
+++ realwork/realworkTests/ProfileTest/ProfileRepoTest.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileRepoTest.swift
+//  realworkTests
+//
+//  Created by Nathan Ngobale on 2020/04/20.
+//  Copyright © 2020 Nathan Ngobale. All rights reserved.
+//
+
+import Foundation
---
+++ realwork/realworkUITests/ScreenTests/ProfileScreenTests.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileScreenTests.swift
+//  realworkUITests
+//
+//  Created by Nathan Ngobale on 2020/04/20.
+//  Copyright © 2020 Nathan Ngobale. All rights reserved.
+//
+
+import Foundation
